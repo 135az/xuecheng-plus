1045794db4e5e1eb347b5c7b9e732ba8d4f3060f
yanjiazheng <1453562554@qq.com>
2025-03-08 16:46:48 +0800

    查询课程计划接口设计

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/TeachplanService.java
@@ -0,0 +1,23 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.content.model.dto.TeachplanDto;
+
+import java.util.List;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 课程基本信息管理业务接口
+ * @date 2022/9/6 21:42
+ */
+public interface TeachplanService {
+
+    /**
+     * @param courseId 课程id
+     * @return List<TeachplanDto>
+     * @description 查询课程计划树型结构
+     * @author Mr.M
+     * @date 2022/9/9 11:13
+     */
+    public List<TeachplanDto> findTeachplanTree(long courseId);
+}
